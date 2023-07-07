23b10e61cd4927945d6211a1667264a21d5fc2bc
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-07-07 17:07:18 +0100

    voter model modified & voter controller created

---
+++ controllers/voterController.js
@@ -0,0 +1,24 @@
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const Voter = require("../models/voterModel");
+
+exports.register = asyncHandler(async (req, res) => {
+  const { name, email, password, role } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+  const existingVoter = await User.findOne({ email });
+  if (existingVoter) {
+    res.status(400).json({ message: "User already exists! Login instead" });
+  }
+
+  //Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const voter = await Voter.create({
+    name,
+    email,
+    password: hashedPassword,
+    role,
+  });
+});
